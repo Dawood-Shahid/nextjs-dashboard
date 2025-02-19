1b8726ac40f521fe1d6b70be7af80221861fc466
Dawood-Shahid <dawoodshahid67@gmail.com>
2025-02-20 00:43:37 +0500

    feat: update invoice status colors for better visibility

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -7,21 +7,21 @@ export default function InvoiceStatus({ status }: { status: string }) {
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-red-500 text-white': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
-        },
+        }
       )}
     >
       {status === 'pending' ? (
         <>
           Pending
-          <ClockIcon className="ml-1 w-4 text-gray-500" />
+          <ClockIcon className='ml-1 w-4 text-white' />
         </>
       ) : null}
       {status === 'paid' ? (
         <>
           Paid
-          <CheckIcon className="ml-1 w-4 text-white" />
+          <CheckIcon className='ml-1 w-4 text-white' />
         </>
       ) : null}
     </span>
